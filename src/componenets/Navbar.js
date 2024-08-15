9691f2188b78b488adcbe43960e3c9b658bce214
Musaddique12 <musaddiquemomin018@gmail.com>
2024-08-15 16:36:21 +0530

    not compete

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './all.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate=useNavigate()
   return (
     <nav className="navbar">
       <h1>Quiz Website</h1>
+      <Link 
+  to='/signup' 
+  style={{ 
+    marginRight: '10px', 
+    padding: '8px 16px', 
+    backgroundColor: '#4CAF50', 
+    color: 'white', 
+    textDecoration: 'none', 
+    borderRadius: '4px' 
+  }}
+>
+  Signup
+</Link>
+
+<Link 
+  to='/login' 
+  style={{ 
+    padding: '8px 16px', 
+    backgroundColor: '#4CAF50', 
+    color: 'white', 
+    textDecoration: 'none', 
+    borderRadius: '4px' 
+  }}
+>
+  Login
+</Link>
+
     </nav>
   );
 }
